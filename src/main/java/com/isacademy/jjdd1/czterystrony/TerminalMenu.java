c7c07d74939c28b82ccfc092e42d4d65291a4a38
jakubdobosz <jakub.dobosz@outlook.com>
2017-03-31 16:39:39 +0200

    Add text

---
+++ src/main/java/com/isacademy/jjdd1/czterystrony/TerminalMenu.java
@@ -0,0 +1,13 @@
+package com.isacademy.jjdd1.czterystrony;
+
+/**
+ * Created by jakubdobosz on 31.03.17.
+ */
+public class TerminalMenu {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+
+
+    }
+}
