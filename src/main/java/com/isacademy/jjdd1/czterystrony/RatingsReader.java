7f3b2d8059863d3e0455596d932c985a99f1301f
jakubdobosz <jakub.dobosz@outlook.com>
2017-04-03 15:35:52 +0200

    add RatingsReader class

---
+++ src/main/java/com/isacademy/jjdd1/czterystrony/RatingsReader.java
@@ -0,0 +1,25 @@
+package com.isacademy.jjdd1.czterystrony;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.io.IOException;;
+
+public class RatingsReader {
+
+    private static final String SEPARATOR = ",";
+
+    private final String DIRECTORY = "/home/jakubdobosz/Downloads/omegafun";
+
+    private Path filePath;
+
+    public RatingsReader(String investFundID) {
+        this.filePath = Paths.get(DIRECTORY, investFundID);
+    }
+
+    public List<String> getContentList() throws IOException {
+        return Files.readAllLines(filePath, StandardCharsets.UTF_8);
+    }
+}
--- src/main/java/com/isacademy/jjdd1/czterystrony/TerminalMenu.java
+++ src/main/java/com/isacademy/jjdd1/czterystrony/TerminalMenu.java
@@ -1,8 +1,10 @@
 package com.isacademy.jjdd1.czterystrony;
 
+import java.util.List;
+
 public class TerminalMenu {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+        RatingsReader newTestFileReader = new RatingsReader("AGI001.txt");
     }
 }
