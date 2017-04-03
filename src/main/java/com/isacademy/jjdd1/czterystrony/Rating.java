fd2fa9d505e542e2e024dae8b25592da829afacc
jakubdobosz <jakub.dobosz@outlook.com>
2017-04-03 16:28:21 +0200

    add Rating class

---
+++ src/main/java/com/isacademy/jjdd1/czterystrony/Rating.java
@@ -0,0 +1,51 @@
+package com.isacademy.jjdd1.czterystrony;
+
+import java.util.Date;
+
+public class Rating {
+    private Date date;
+    private Double high;
+    private Double low;
+    private Double close;
+    private Double volume;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Double getHigh() {
+        return high;
+    }
+
+    public void setHigh(Double high) {
+        this.high = high;
+    }
+
+    public Double getLow() {
+        return low;
+    }
+
+    public void setLow(Double low) {
+        this.low = low;
+    }
+
+    public Double getClose() {
+        return close;
+    }
+
+    public void setClose(Double close) {
+        this.close = close;
+    }
+
+    public Double getVolume() {
+        return volume;
+    }
+
+    public void setVolume(Double volume) {
+        this.volume = volume;
+    }
+}
