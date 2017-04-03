7d5a7619da5d573838e7228554c9fe17cbc37ef1
jakubdobosz <jakub.dobosz@outlook.com>
2017-04-03 10:42:13 +0200

    set maven

--- src/main/java/com/isacademy/jjdd1/czterystrony/TerminalMenu.java
+++ src/main/java/com/isacademy/jjdd1/czterystrony/TerminalMenu.java
@@ -1,13 +1,8 @@
 package com.isacademy.jjdd1.czterystrony;
 
-/**
- * Created by jakubdobosz on 31.03.17.
- */
 public class TerminalMenu {
 
     public static void main(String[] args) {
         System.out.println("hello");
-
-
     }
 }
