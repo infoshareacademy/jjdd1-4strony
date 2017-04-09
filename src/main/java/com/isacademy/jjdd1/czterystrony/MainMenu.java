32ee44deb09405c4d42cc18868edf7281dc27475
Jakub <jakub.dobosz@outlook.com>
2017-04-09 22:15:08 +0200

    add psvm in MainMenu

--- src/main/java/com/isacademy/jjdd1/czterystrony/MainMenu.java
+++ src/main/java/com/isacademy/jjdd1/czterystrony/MainMenu.java
@@ -3,8 +3,7 @@ package com.isacademy.jjdd1.czterystrony;
 import java.util.Scanner;
 
 public class MainMenu {
-        public int MainMenu; {
-
+    public static void main(String[] args) {
         System.out.println("Witaj w analizatorze funduszy inwestycyjnych. Wybierz co chcesz zrobić:");
         System.out.println("[1]. Rozpocznij");
         System.out.println("[0]. Wyjdź");
