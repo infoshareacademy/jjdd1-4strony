575ee90f36d0ea3d2852745b51bf063800a6d7a4
Jakub <jakub.dobosz@outlook.com>
2017-04-09 16:22:51 +0200

    add private static fields in RatingFactory class

--- src/main/java/com/isacademy/jjdd1/czterystrony/ChoosingDateMenu.java
+++ src/main/java/com/isacademy/jjdd1/czterystrony/ChoosingDateMenu.java
@@ -1,14 +1,7 @@
 package com.isacademy.jjdd1.czterystrony;
 
-
 public class ChoosingDateMenu {
-
     public static String choosingDateMenu; {
-
-
         System.out.println("Test choosingMenu");
     }
-
-
-
 }
