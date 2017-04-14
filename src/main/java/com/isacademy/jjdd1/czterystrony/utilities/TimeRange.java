6089888a8aa82f942054f3a205e18e33f889eec7
Jakub <jakub.dobosz@outlook.com>
2017-04-14 21:40:33 +0200

    add TimeRange class

---
+++ src/main/java/com/isacademy/jjdd1/czterystrony/utilities/TimeRange.java
@@ -0,0 +1,19 @@
+package com.isacademy.jjdd1.czterystrony.utilities;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class TimeRange {
+    private LocalDate start;
+    private LocalDate end;
+
+    public TimeRange(LocalDate start, LocalDate end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public int getDifferenceInDays() {
+        return Period.between(start, end).getDays();
+    }
+
+}
