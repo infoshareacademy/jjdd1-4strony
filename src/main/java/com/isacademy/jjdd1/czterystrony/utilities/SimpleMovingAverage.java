e90b9acb0cc848002afa62568c9b8de9b59631e4
Jakub <jakub.dobosz@outlook.com>
2017-04-15 00:23:28 +0200

    implement SimpleMovingAverage

--- src/main/java/com/isacademy/jjdd1/czterystrony/utilities/SimpleMovingAverage.java
+++ src/main/java/com/isacademy/jjdd1/czterystrony/utilities/SimpleMovingAverage.java
@@ -1,5 +1,31 @@
 package com.isacademy.jjdd1.czterystrony.utilities;
 
-public class SimpleMovingAverage {
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class SimpleMovingAverage {
+    private final Queue<BigDecimal> window = new LinkedList<>();
+    private final int period;
+    private BigDecimal sum = BigDecimal.ZERO;
+
+    public SimpleMovingAverage(int period) {
+        assert period > 0 : "Period must be a positive integer";
+        this.period = period;
+    }
+
+    public void add(BigDecimal value) {
+        sum = sum.add(value);
+        window.add(value);
+        if (window.size() > period) {
+            sum = sum.subtract(window.remove());
+        }
+    }
+
+    public BigDecimal getAverage() {
+        if (window.isEmpty()) return BigDecimal.ZERO;
+        BigDecimal divisor = BigDecimal.valueOf(window.size());
+        return sum.divide(divisor, 2, RoundingMode.HALF_UP);
+    }
 }
