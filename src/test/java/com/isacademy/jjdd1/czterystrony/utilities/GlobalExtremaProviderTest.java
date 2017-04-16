7c8547e6eab6f3ef73939527d3394e0b5b383065
jakub-dobosz <jakub.dobosz@outlook.com>
2017-04-16 17:57:21 +0200

    add tests in SimpleMovingAverageTest

--- src/test/java/com/isacademy/jjdd1/czterystrony/utilities/GlobalExtremaProviderTest.java
+++ src/test/java/com/isacademy/jjdd1/czterystrony/utilities/GlobalExtremaProviderTest.java
@@ -1,4 +1,23 @@
 package com.isacademy.jjdd1.czterystrony.utilities;
 
+import com.isacademy.jjdd1.czterystrony.dao.InvestFundsDaoTxt;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
 public class GlobalExtremaProviderTest {
+
+    private static InvestFundsDaoTxt investFundsDaoTxt;
+    private static GlobalExtremaProvider globalExtremaProvider;
+
+    @Before
+    public void setup() {
+//        globalExtremaProvider = new GlobalExtremaProvider()
+    }
+
+    @Test
+    public void method_name() {
+//        assertThat()
+    }
 }
--- src/test/java/com/isacademy/jjdd1/czterystrony/utilities/SimpleMovingAverageTest.java
+++ src/test/java/com/isacademy/jjdd1/czterystrony/utilities/SimpleMovingAverageTest.java
@@ -1,4 +1,32 @@
 package com.isacademy.jjdd1.czterystrony.utilities;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.*;
+
 public class SimpleMovingAverageTest {
+
+    private static final int DIGITS_AFTER_COMMA = 2;
+    private static final int period = 5;
+    private static SimpleMovingAverage simpleMovingAverage;
+
+    @Before
+    public void setup() {
+        simpleMovingAverage = new SimpleMovingAverage(period);
+    }
+
+    @Test
+    public void should_get_average_equal_to_zero() {
+        assertThat(simpleMovingAverage.getAverage()).isEqualTo(BigDecimal.ZERO);
+    }
+
+    @Test
+    public void average_should_be_equal_to_added_value() {
+        BigDecimal value = BigDecimal.valueOf(10, DIGITS_AFTER_COMMA);
+        simpleMovingAverage.add(value);
+        assertThat(simpleMovingAverage.getAverage()).isEqualTo(value);
+    }
 }
