40ada88e396bd5f4c9793e7f66dcc095aa9861e8
jakub-dobosz <jakub.dobosz@outlook.com>
2017-04-17 13:07:53 +0200

    refactor InvestFundDaoTxt data access object

--- src/main/java/com/isacademy/jjdd1/czterystrony/instruments/InvestFundFactory.java
+++ src/main/java/com/isacademy/jjdd1/czterystrony/instruments/InvestFundFactory.java
@@ -2,21 +2,23 @@ package com.isacademy.jjdd1.czterystrony.instruments;
 
 import com.isacademy.jjdd1.czterystrony.dao.InvestFundsDao;
 import com.isacademy.jjdd1.czterystrony.utilities.TextFileReader;
+
 import java.io.File;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class InvestFundFactory {
-    public static InvestFund create(String ratingsDataFileName, String investFundName) {
-        String company = investFundName.replaceAll(" .+$", "");
-        File investFundDataFile = new File(InvestFundsDao.INVEST_FUNDS_DATA_FOLDER_DIRECTORY + "/" + ratingsDataFileName);
-        TextFileReader textFileReader = new TextFileReader(investFundDataFile);
+    public static InvestFund create(String dataFileNameWithExtension, String name) {
+        String id = dataFileNameWithExtension.replaceFirst("(\\w+).*", "$1");
+        String company = name.replaceAll(" .+$", "");
+        File dataFile = new File(InvestFundsDao.INVEST_FUNDS_DATA_FOLDER_DIRECTORY + "/" + dataFileNameWithExtension);
+        TextFileReader textFileReader = new TextFileReader(dataFile);
         List<String> records = textFileReader.getContent();
         List<Rating> ratings = getRatings(records);
 
         return new InvestFund.Builder()
-                .withId(ratingsDataFileName)
-                .withName(investFundName)
+                .withId(id)
+                .withName(name)
                 .withCompany(company)
                 .withRatings(ratings)
                 .build();
