357a0993d64f7431b82962d7d7888d2c009dc81f
karol <karol.sitkowski@gmail.com>
2017-04-19 16:00:15 +0200

    Hard coded priority fund listed as first one

--- ifa-library/src/main/java/com/isacademy/jjdd1/czterystrony/ui/MenuOfFunds.java
+++ ifa-library/src/main/java/com/isacademy/jjdd1/czterystrony/ui/MenuOfFunds.java
@@ -5,23 +5,35 @@ import com.isacademy.jjdd1.czterystrony.dao.InvestFundsDao;
 import com.isacademy.jjdd1.czterystrony.dao.InvestFundsDaoTxt;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class MenuOfFunds {
     public int menuOfFunds;
 
     {
         InvestFundsDaoTxt investFundDao = new InvestFundsDaoTxt();
+        List<InvestFund> allByName = investFundDao.getAllByName();
+
 
         System.out.println("Lista funduszy: ");
-        for (InvestFund investFund : investFundDao.getAllByName()) {
+        for (InvestFund investFund : allByName) {
+            if (investFund.getName().equals("AVIVA Obligacji")) {
+                investFund.promote(99);
+            }
+        }
+        for (InvestFund investFund : allByName.stream()
+                .sorted(Comparator.comparing(InvestFund::getPriority))
+                .collect(Collectors.toList())) {
             System.out.println(investFund.getName());
         }
-
         System.out.println("Wpisz nazwę wybranego funduszu, aby przejść dalej:");
         Scanner choice = new Scanner(System.in);
         String fund = choice.nextLine();//fundusz wybrany przez użytkownika
         InvestFund investFund = investFundDao.get(fund);
         MenuOfExtreme menuExtreme = new MenuOfExtreme(investFund);
+
+
+
 //        investFund.getAllRatings();
 
 
