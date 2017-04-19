bd49d74c564ebd43cd068e1d8203ef225de43705
karol <karol.sitkowski@gmail.com>
2017-04-19 15:54:54 +0200

    repair MenuOfFunds class

--- ifa-library/src/main/java/com/isacademy/jjdd1/czterystrony/ui/MenuOfFunds.java
+++ ifa-library/src/main/java/com/isacademy/jjdd1/czterystrony/ui/MenuOfFunds.java
@@ -10,21 +10,21 @@ public class MenuOfFunds {
     public int menuOfFunds;
 
     {
-//        InvestFundsDaoTxt investFundDao = new InvestFundsDaoTxt();
+        InvestFundsDaoTxt investFundDao = new InvestFundsDaoTxt();
 
-//        System.out.println("Lista funduszy: ");
-//        for (InvestFund investFund : investFundDao.getAllByName()) {
-//            System.out.println(investFund.getName());
-//        }
+        System.out.println("Lista funduszy: ");
+        for (InvestFund investFund : investFundDao.getAllByName()) {
+            System.out.println(investFund.getName());
+        }
 
-//        System.out.println("Wpisz nazwę wybranego funduszu, aby przejść dalej:");
-//        Scanner choice = new Scanner(System.in);
-//        String fund = choice.nextLine();//fundusz wybrany przez użytkownika
-//        InvestFund investFund = investFundDao.get(fund);
-//        MenuOfExtreme menuExtreme = new MenuOfExtreme(investFund);
+        System.out.println("Wpisz nazwę wybranego funduszu, aby przejść dalej:");
+        Scanner choice = new Scanner(System.in);
+        String fund = choice.nextLine();//fundusz wybrany przez użytkownika
+        InvestFund investFund = investFundDao.get(fund);
+        MenuOfExtreme menuExtreme = new MenuOfExtreme(investFund);
 //        investFund.getAllRatings();
-//
-//
+
+
 //        for (String fundChoice : allInvestFunds.keySet()) {
 //            System.out.println();
 //        }
