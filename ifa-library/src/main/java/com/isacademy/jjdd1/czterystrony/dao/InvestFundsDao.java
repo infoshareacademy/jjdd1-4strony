d84be58a74b53e9b37714fe60cace448ee72a1fd
jakub-dobosz <jakub.dobosz@outlook.com>
2017-04-28 23:20:08 +0200

    delete YAGNIsh interface StockExchangeDao

--- ifa-library/src/main/java/com/isacademy/jjdd1/czterystrony/dao/InvestFundsDao.java
+++ ifa-library/src/main/java/com/isacademy/jjdd1/czterystrony/dao/InvestFundsDao.java
@@ -5,12 +5,10 @@ import com.isacademy.jjdd1.czterystrony.instruments.InvestFund;
 import java.io.FileNotFoundException;
 import java.util.List;
 
-public interface InvestFundsDao extends StockExchangeDao<InvestFund> {
+public interface InvestFundsDao {
 
-    @Override
     InvestFund get(String id) throws FileNotFoundException;
 
-    @Override
     List<InvestFund> getAllByName() throws FileNotFoundException;
 
     List<InvestFund> getAllByPriority() throws FileNotFoundException;
