3d349640249bd84833ebf923ceda3374bbc1b332
jakub-dobosz <jakub.dobosz@outlook.com>
2017-05-02 23:32:21 +0200

    split web filters

--- ifa-webapp/src/main/java/com/isacademy/jjdd1/czterystrony/filters/InvestFundIdFilter.java
+++ ifa-webapp/src/main/java/com/isacademy/jjdd1/czterystrony/filters/InvestFundIdFilter.java
@@ -1,16 +1,21 @@
 package com.isacademy.jjdd1.czterystrony.filters;
 
+import com.isacademy.jjdd1.czterystrony.dbviews.Views;
+import com.isacademy.jjdd1.czterystrony.model.InvestFundDetails;
+
+import javax.inject.Inject;
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.time.LocalDate;
-import java.time.format.DateTimeParseException;
-import java.util.Objects;
+import java.util.Optional;
 
 @WebFilter(urlPatterns = {"/4analysis/notowania/*", "/4analysis/analiza/*"})
-public class TimeRangeFilter implements Filter {
+public class InvestFundIdFilter implements Filter {
+
+    @Inject
+    Views views;
 
     @Override
     public void doFilter(ServletRequest request,
@@ -20,29 +25,18 @@ public class TimeRangeFilter implements Filter {
         HttpServletResponse httpServletResponse = (HttpServletResponse) response;
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
 
-        if (!timeRangeIsValid(httpServletRequest)) {
-            httpServletResponse.sendRedirect(httpServletRequest.getRequestURL().toString());
+        if (!fundIdIsValid(httpServletRequest)) {
+            httpServletResponse.sendRedirect("http://localhost:8080/4analysis");
             return;
         }
 
         chain.doFilter(request, response);
     }
 
-    private boolean timeRangeIsValid(HttpServletRequest request) {
-        String from = request.getParameter("from");
-        String to = request.getParameter("to");
-        return dateIsValid(from) && dateIsValid(to);
-    }
-
-    private boolean dateIsValid(String date) {
-        if (!Objects.isNull(date) && !date.isEmpty()) {
-            try {
-                LocalDate.parse(date);
-            } catch (DateTimeParseException e) {
-                return false;
-            }
-        }
-        return true;
+    private boolean fundIdIsValid(HttpServletRequest request) {
+        String investFundId = request.getPathInfo().substring(1);
+        Optional<InvestFundDetails> investFundDetailsOptional = views.getById(investFundId);
+        return investFundDetailsOptional.isPresent();
     }
 
     @Override
