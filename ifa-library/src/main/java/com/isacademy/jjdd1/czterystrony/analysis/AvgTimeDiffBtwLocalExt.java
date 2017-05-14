36f8514db01dbb64bc4f14eeb695f2e74c3fd308
maciejwolodko <maciejwolodko@wp.pl>
2017-05-14 20:16:50 +0200

    shortening method and remove unnessery exception, type optimization, call methods

--- ifa-library/src/main/java/com/isacademy/jjdd1/czterystrony/analysis/AvgTimeDiffBtwLocalExt.java
+++ ifa-library/src/main/java/com/isacademy/jjdd1/czterystrony/analysis/AvgTimeDiffBtwLocalExt.java
@@ -1,47 +1,31 @@
 package com.isacademy.jjdd1.czterystrony.analysis;
 
-import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.*;
 
 
 public class AvgTimeDiffBtwLocalExt {
 
+
     private List<Integer> listOfDifferenceTime = new ArrayList<>();
 
-    private List<String> listOfDates = new ArrayList<>();
-
-    private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
+    private List<LocalDate> listOfDates = new ArrayList<>();
 
 
-    public static int getDayCount(String start, String end) {
-        int diff = -1;
-        try {
-            Date dateStart = simpleDateFormat.parse(start);
-            Date dateEnd = simpleDateFormat.parse(end);
+    public long getDaysCount(LocalDate start, LocalDate end) {
 
-            diff = (int) Math.round((dateEnd.getTime() - dateStart.getTime()) / (double) 86400000);
-        } catch (Exception e) {
-
-        }
-        return diff;
+        return Math.abs(ChronoUnit.DAYS.between(start, end));
     }
-
 
     public List<Integer> calculationDiffMeanTime() {
 
 
         for (int i = 0; i < listOfDates.size() - 1; i++) {
 
-            Integer differenceInPositive;
-            Integer differenceValue = getDayCount(listOfDates.get(i), listOfDates.get(i + 1));
 
-            if (differenceValue < 0) {
-                differenceInPositive = differenceValue * (-1);
-                listOfDifferenceTime.add(i, differenceInPositive);
-            } else {
+            listOfDifferenceTime.add(i, (int) getDaysCount(listOfDates.get(i), listOfDates.get(i + 1)));
 
-                listOfDifferenceTime.add(i, differenceValue);
-            }
 
         }
         return listOfDifferenceTime;
@@ -55,26 +39,43 @@ public class AvgTimeDiffBtwLocalExt {
         }
 
         int sum = 0;
-        for (long mark : listOfDifferenceTime) {
+        for (Integer mark : listOfDifferenceTime) {
             sum += mark;
 
         }
+        System.out.println("Suma tych różnic to: " + sum);
+        System.out.println("Średnie różnice między datami " + sum / listOfDifferenceTime.size() + " dni.");
 
         return sum / listOfDifferenceTime.size();
     }
-
 
     public List<Integer> getListOfDifferenceTime() {
         return listOfDifferenceTime;
     }
 
-    public List<String> getListOfDates() {
+
+    public static void main(String[] args) {
+
+        AvgTimeDiffBtwLocalExt avgTimeDiffBtwLocalExt = new AvgTimeDiffBtwLocalExt();
+        avgTimeDiffBtwLocalExt.fillTheList();
+        System.out.println(avgTimeDiffBtwLocalExt.calculationDiffMeanTime());
+        avgTimeDiffBtwLocalExt.calculateAvgDiffBtnTime(avgTimeDiffBtwLocalExt.listOfDifferenceTime);
+    }
+
+
+    public List<LocalDate> fillTheList() {
+
+        listOfDates.add(LocalDate.ofEpochDay(2010 - 02 - 23));
+        listOfDates.add(LocalDate.ofEpochDay(2010 - 02 - 25));
+        listOfDates.add(LocalDate.ofEpochDay(2010 - 02 - 22));
+        listOfDates.add(LocalDate.ofEpochDay(2010 - 02 - 01));
+        listOfDates.add(LocalDate.ofEpochDay(2010 - 02 - 23));
+        listOfDates.add(LocalDate.ofEpochDay(2010 - 02 - 03));
+        listOfDates.add(LocalDate.ofEpochDay(2010 - 02 - 27));
+
         return listOfDates;
     }
 
-    public static SimpleDateFormat getSimpleDateFormat() {
-        return simpleDateFormat;
-    }
 }
 
 
