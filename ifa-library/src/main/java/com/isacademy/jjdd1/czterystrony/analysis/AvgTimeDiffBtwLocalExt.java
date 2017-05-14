902a3947e32eb02a43241dcef100f1e0c4fb59f0
maciejwolodko <maciejwolodko@wp.pl>
2017-05-14 20:19:31 +0200

    clean

--- ifa-library/src/main/java/com/isacademy/jjdd1/czterystrony/analysis/AvgTimeDiffBtwLocalExt.java
+++ ifa-library/src/main/java/com/isacademy/jjdd1/czterystrony/analysis/AvgTimeDiffBtwLocalExt.java
@@ -43,37 +43,11 @@ public class AvgTimeDiffBtwLocalExt {
             sum += mark;
 
         }
-        System.out.println("Suma tych różnic to: " + sum);
-        System.out.println("Średnie różnice między datami " + sum / listOfDifferenceTime.size() + " dni.");
-
         return sum / listOfDifferenceTime.size();
     }
 
     public List<Integer> getListOfDifferenceTime() {
         return listOfDifferenceTime;
-    }
-
-
-    public static void main(String[] args) {
-
-        AvgTimeDiffBtwLocalExt avgTimeDiffBtwLocalExt = new AvgTimeDiffBtwLocalExt();
-        avgTimeDiffBtwLocalExt.fillTheList();
-        System.out.println(avgTimeDiffBtwLocalExt.calculationDiffMeanTime());
-        avgTimeDiffBtwLocalExt.calculateAvgDiffBtnTime(avgTimeDiffBtwLocalExt.listOfDifferenceTime);
-    }
-
-
-    public List<LocalDate> fillTheList() {
-
-        listOfDates.add(LocalDate.ofEpochDay(2010 - 02 - 23));
-        listOfDates.add(LocalDate.ofEpochDay(2010 - 02 - 25));
-        listOfDates.add(LocalDate.ofEpochDay(2010 - 02 - 22));
-        listOfDates.add(LocalDate.ofEpochDay(2010 - 02 - 01));
-        listOfDates.add(LocalDate.ofEpochDay(2010 - 02 - 23));
-        listOfDates.add(LocalDate.ofEpochDay(2010 - 02 - 03));
-        listOfDates.add(LocalDate.ofEpochDay(2010 - 02 - 27));
-
-        return listOfDates;
     }
 
 }
