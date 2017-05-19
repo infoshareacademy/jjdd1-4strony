b280de3aca21690c31f6ab7a02f093c2b4db2142
maciejwolodko <maciejwolodko@wp.pl>
2017-05-19 05:54:14 +0200

    adding fields to show averageDataDifference and averageValueDifference Local Extreme

--- ifa-webapp/src/main/java/com/isacademy/jjdd1/czterystrony/model/StatisticsDetails.java
+++ ifa-webapp/src/main/java/com/isacademy/jjdd1/czterystrony/model/StatisticsDetails.java
@@ -1,15 +1,24 @@
 package com.isacademy.jjdd1.czterystrony.model;
 
+import java.math.BigDecimal;
+
+
 public class StatisticsDetails {
+
 
     private String name;
     private String id;
     private int clicks;
+    private int averageDataDifference;
+    private BigDecimal averageValueDifferenceBetweenLocalExtrema;
 
-    public StatisticsDetails(String name, String id, int clicks) {
+
+    public StatisticsDetails(String name, String id, int clicks, int averageDataDifference, BigDecimal averageValueDifferenceBetweenLocalExtrema) {
         this.name = name;
         this.id = id;
         this.clicks = clicks;
+        this.averageDataDifference = averageDataDifference;
+        this.averageValueDifferenceBetweenLocalExtrema = averageValueDifferenceBetweenLocalExtrema;
     }
 
     public String getName() {
@@ -23,4 +32,30 @@ public class StatisticsDetails {
     public int getClicks() {
         return clicks;
     }
+
+    public int getAverageDataDifference() {
+        return averageDataDifference;
+    }
+
+    public BigDecimal getAverageValueDifferenceBetweenLocalExtrema() {
+        return averageValueDifferenceBetweenLocalExtrema;
+    }
+
+/*public StatisticsDetails(String name, String id, int clicks) {
+        this.name = name;
+        this.id = id;
+        this.clicks = clicks;
+    }*/
+
+  /*  public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getClicks() {
+        return clicks;
+    }*/
 }
