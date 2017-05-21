358d95077781d7679142c77dfa1c7ac81cb1ba58
jakub-dobosz <jakub.dobosz@outlook.com>
2017-05-21 14:29:38 +0200

    delete @Table annotations

--- ifa-reports/src/main/java/com/isacademy/jjdd1/czterystrony/reports/model/InvestFundsPopularity.java
+++ ifa-reports/src/main/java/com/isacademy/jjdd1/czterystrony/reports/model/InvestFundsPopularity.java
@@ -1,9 +1,7 @@
 package com.isacademy.jjdd1.czterystrony.reports.model;
 
 import javax.persistence.Entity;
-import javax.persistence.Table;
 
 @Entity
-@Table(name = "INVEST_FUNDS_POPULARITY")
 public class InvestFundsPopularity extends Popularity {
 }
--- ifa-reports/src/main/java/com/isacademy/jjdd1/czterystrony/reports/model/PensionFundsPopularity.java
+++ ifa-reports/src/main/java/com/isacademy/jjdd1/czterystrony/reports/model/PensionFundsPopularity.java
@@ -1,9 +1,7 @@
 package com.isacademy.jjdd1.czterystrony.reports.model;
 
 import javax.persistence.Entity;
-import javax.persistence.Table;
 
 @Entity
-@Table(name = "PENSION_FUNDS_POPULARITY")
 public class PensionFundsPopularity extends Popularity {
 }
