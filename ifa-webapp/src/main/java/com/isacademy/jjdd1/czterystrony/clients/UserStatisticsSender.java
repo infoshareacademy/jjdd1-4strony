97dba30475303ab96730f9a5c04da2a053c1daa1
jakub-dobosz <jakub.dobosz@outlook.com>
2017-05-28 23:42:00 +0200

    add UserStatisticsSender class

---
+++ ifa-webapp/src/main/java/com/isacademy/jjdd1/czterystrony/clients/UserStatisticsSender.java
@@ -0,0 +1,30 @@
+package com.isacademy.jjdd1.czterystrony.clients;
+
+import isacademy.jjdd1.czterystrony.reports.persistence.model.statistics.UserStatistics;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ejb.Asynchronous;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import java.time.LocalDateTime;
+
+@Stateless
+public class UserStatisticsSender {
+    private static final Logger log = LoggerFactory.getLogger(UserStatisticsSender.class);
+
+    @Inject
+    UserStatisticsClient client;
+
+    @Asynchronous
+    public void send(String name, String email) {
+        UserStatistics statistics = new UserStatistics.Builder()
+                .withName(name)
+                .withEmail(email)
+                .withLoginDateTime(LocalDateTime.now())
+                .build();
+
+        client.post(statistics);
+        log.info("User {} logged in. Statistics sent to report module");
+    }
+}
